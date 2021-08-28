aa232c9f349723e6d43de9be7167aab8164ae546
Ovidiu Golban <68772875+OviGolban@users.noreply.github.com>
2021-08-28 18:44:10 +0300

    Delete Comparare.java

--- Comparare.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class Comparare implements Comparator<Monom> {
-    public int compare(Monom m1, Monom m2) {
-        return m2.getGrad() - m1.getGrad();
-    }
-}
